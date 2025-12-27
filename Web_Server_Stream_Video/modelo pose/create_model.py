a01e764b64b40effff4689839b01de6709920ec5
Bojtronic <bojtronic@gmail.comc>
2025-12-27 13:50:47 -0600

    Preparación de entornos y requerimientos para la creación, entrenamiento e implementación de un modelo que procese los landmarks de mediapipe para clasificar las poses

--- Web_Server_Stream_Video/modelo pose/create_model.py
+++ Web_Server_Stream_Video/modelo pose/create_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from ..detectors.person_detector import PersonDetector
 
 data = pd.read_csv("pose_dataset.csv")
 
